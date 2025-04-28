39e0e61d5bd327bb8b973d31b62f11e92ab1a1ad
FelipeDePaulaRosa <felipedepaularosa11@gmail.com>
2025-04-28 19:20:33 -0300

    Remove unnecessary changes on migration that were created before in other migrations

--- src/Infrastructure/Migrations/20250428123702_CreateFaq.cs
+++ src/Infrastructure/Migrations/20250428123702_CreateFaq.cs
@@ -11,26 +11,6 @@ namespace Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_user_UserType_UserTypeId",
-                schema: "public",
-                table: "user");
-
-            migrationBuilder.DropTable(
-                name: "UserType",
-                schema: "public");
-
-            migrationBuilder.DropIndex(
-                name: "IX_user_UserTypeId",
-                schema: "public",
-                table: "user");
-
-            migrationBuilder.RenameColumn(
-                name: "UserTypeId",
-                schema: "public",
-                table: "user",
-                newName: "type");
-
             migrationBuilder.CreateTable(
                 name: "faq",
                 schema: "public",
@@ -70,39 +50,6 @@ namespace Infrastructure.Migrations
             migrationBuilder.DropTable(
                 name: "faq",
                 schema: "public");
-
-            migrationBuilder.RenameColumn(
-                name: "type",
-                schema: "public",
-                table: "user",
-                newName: "UserTypeId");
-
-            migrationBuilder.CreateTable(
-                name: "UserType",
-                schema: "public",
-                columns: table => new
-                {
-                    UserTypeId = table.Column<int>(type: "integer", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_UserType", x => x.UserTypeId);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_user_UserTypeId",
-                schema: "public",
-                table: "user",
-                column: "UserTypeId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_user_UserType_UserTypeId",
-                schema: "public",
-                table: "user",
-                column: "UserTypeId",
-                principalSchema: "public",
-                principalTable: "UserType",
-                principalColumn: "UserTypeId");
         }
     }
 }
