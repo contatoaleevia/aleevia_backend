7f929a274fa1ffdc096fa71985ee810a735c9908
Jonathan Silveira <jonathansouzadasilveira17@gmail.com>
2025-05-10 01:16:06 -0300

    Fix: Update CreateOffice method to return CreateOfficeResponse

--- src/Application/Services/Offices/IOfficeService.cs
+++ src/Application/Services/Offices/IOfficeService.cs
@@ -7,7 +7,7 @@ namespace Application.Services.Offices;
 
 public interface IOfficeService
 {
-    Task<Guid> CreateOffice(CreateOfficeRequest request, Guid userId);
+    Task<CreateOfficeResponse> CreateOffice(CreateOfficeRequest request, Guid userId);
     Task<Guid> BindOfficeAddress(BindOfficeAddressRequest request, Guid userId);
     Task DeleteOfficeAddress(Guid officeAddressId);
     Task<Guid> BindOfficeProfessional(BindOfficeProfessionalRequest request);
