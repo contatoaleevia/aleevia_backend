a13d4a66bc028cf13859ad784e27d4389c8eaad6
Marcus Rodrigues <marcus1751@outlook.com>
2025-05-10 16:36:14 -0300

    Feat: Enhance GetAllActiveAsync method in ProfessionRepository with ordering and split query

--- src/Infrastructure/Repositories/ProfessionRepository.cs
+++ src/Infrastructure/Repositories/ProfessionRepository.cs
@@ -13,10 +13,15 @@ public class ProfessionRepository(ApiDbContext context) : Repository<Profession>
     public async Task<List<Profession>> GetAllActiveAsync()
     {
         return await DbSet
-            .Include(p => p.Specialties.Where(s => s.Active))
-            .ThenInclude(s => s.SubSpecialties.Where(ss => ss.Active))
             .Where(p => p.Active)
             .OrderBy(p => p.Name)
+            .Include(p => p.Specialties
+                .Where(s => s.Active)
+                .OrderBy(s => s.Name))
+            .ThenInclude(s => s.SubSpecialties
+                .Where(ss => ss.Active)
+                .OrderBy(ss => ss.Name))
+            .AsSplitQuery()
             .ToListAsync();
     }
 
