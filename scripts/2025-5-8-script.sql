3316b13217a6146b9d4ea2b3872ac4e0f0b6b3f5
FelipeDePaulaRosa <felipedepaularosa11@gmail.com>
2025-05-12 11:09:48 -0300

    migrations

---
+++ scripts/2025-5-8-script.sql
@@ -0,0 +1,13 @@
+﻿START TRANSACTION;
+
+ALTER TABLE public.professional ADD email character varying(100);
+
+ALTER TABLE public.professional ADD name character varying(100);
+
+ALTER TABLE public.professional ADD preferred_name character varying(100);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250508235543_UpdateProfessionalAddNameEmail', '8.0.15');
+
+COMMIT;
+
